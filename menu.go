d3c3a4ddff7d9a8c07642d2073a22b348009926d
Travis Harris <travis.harris.development@gmail.com>
2023-02-09 06:11:03 -0500

    loading csv to slice of string

--- menu.go
+++ menu.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Hi")
 	fmt.Println("Loading Menu from csv")
-	csv, err := os.ReadFile("recipes.csv")
+	raw, err := os.ReadFile("recipes.csv")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(string(csv))
+	fmt.Println(string(raw))
+
+	reader := csv.NewReader(strings.NewReader(string(raw)))
+
+	recipes, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(recipes)
 
 }
