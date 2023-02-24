3ec49e0fb6e4b556885954635fdfb0fa07b4281a
Travis Harris <travis.harris.development@gmail.com>
2023-02-23 21:01:24 -0500

    started implementing user choices

--- menu.go
+++ menu.go
@@ -10,6 +10,8 @@ import (
 	"time"
 )
 
+var numDiners = 4
+
 // note to self this remove method does not preserve order
 // lotsa discussion here https://stackoverflow.com/questions/37334119/how-to-delete-an-element-from-a-slice-in-golang
 func remove(s [][]string, i int) [][]string {
@@ -40,7 +42,45 @@ func GetProspectiveRecipes() [][]string {
 	return recipes
 }
 
+func confirmOperation(choice string) {
+	switch choice {
+	case "1":
+		fmt.Println("One Week Menu")
+	case "2":
+		fmt.Println("Menu for x days")
+	case "3":
+		ChangeNumberOfDiners()
+		confirmOperation(getUserChoice())
+	case "q", "Q":
+		os.Exit(0)
+	default:
+		confirmOperation(getUserChoice())
+	}
+}
+
+func ChangeNumberOfDiners() {
+	var input int
+	fmt.Print("How many diners? ")
+	fmt.Scanln(&input)
+	numDiners = input
+	fmt.Println("Okay", numDiners, "for dinner.")
+}
+
+func getUserChoice() string {
+	var input string
+	fmt.Println("\nWelcome to the Menu Generator")
+	fmt.Println("\n1 - Create a menu for one week")
+	fmt.Println("\n2 - Create a menu for a custom number of days")
+	fmt.Printf("\n3 - Change number of diners (currently %d)", numDiners)
+	fmt.Println("\n\nQ to Quit")
+	fmt.Print("\n\nPlease enter your choice:  ")
+	fmt.Scanln(&input)
+	return input
+}
+
 func main() {
+	choice := getUserChoice()
+	confirmOperation(choice)
 
 	recipes := GetProspectiveRecipes()
 
